e5427182d9fc7b1f03d50d60af84fd12114a4678
josephsdavid <david.josephs@beacon.bio>
2022-09-06 20:21:29 +0000

    update and planning

--- config/startup.jl
+++ config/startup.jl
@@ -12,6 +12,8 @@ const keybinds = Dict{Any,Any}(
     "^r" => fzf_hist_search,
 )
 
+# TODO: Hook which runs tests, and then goes into failed tests, activates test environment, and then enters you into a debugger repl (I think i can just use debugger.jl to do something like this)
+
 function customize_keys(repl)
     repl.interface = REPL.setup_interface(repl; extra_repl_keymap = keybinds)
 end
